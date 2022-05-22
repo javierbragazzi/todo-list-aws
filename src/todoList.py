251035bf27be6a1e0015b8102c0ecf23725125e3
javierbragazzi <javierr87@gmail.com>
2022-05-22 19:35:30 +0000

    code style fix

--- src/todoList.py
+++ src/todoList.py
@@ -147,13 +147,17 @@ def create_todo_table(dynamodb):
 
     return table
 
+
 def get_translation(text, language, dynamodb=None):
-    translator = boto3.client(service_name='translate', region_name='us-east-1')
+    translator = boto3.client(service_name='translate',
+    region_name='us-east-1')
     try:
-        translation = translator.translate_text(Text=text, SourceLanguageCode="auto", TargetLanguageCode=language)
+        translation = translator.translate_text(Text=text, 
+        SourceLanguageCode="auto", TargetLanguageCode=language)
     except ClientError as e:  # pragma: no cover
         print(e.response['Error']['Message'])
     else:
         print('Result get_translation:'+str(translation))
         if 'Translation' in translation:
             return translation['Translation']
+            
